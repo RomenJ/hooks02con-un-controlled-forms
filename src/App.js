d539cd1e99d28b07d6af0821a42b50e744246ffc
RomenJ <justiciaterapeutica@gmail.com>
2023-08-26 19:58:17 +0200

    more commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App= ()=>{
+//Componente controlado:se le ha de pasar el valor que va mostrar
+const [value, setValue] = useState ({normal: 'valor inicial normal',texto: 'valor inicial de texto', select: 'chanchofelix', check: false, estado: ''})
+const handleChange=({target})=>{
+    setValue((state)=> ({
+        ...state, 
+        [target.name]: target.type ==='checkbox'
+         ? target.checked 
+         : target.value 
+    }))
+}
+   console.log(value)
+    return(
+        <div>
+            {value.length <=5 ? <span> Longitud mínima 5 caracteres</span> : null}
+            <p> Prueba formulario controlado </p>
+            <input type="text" name="normal" value={value.normal} onChange={handleChange}/>
+            <hr/>
+            <textarea name='texto' onChange={handleChange} value={value.texto} />
+            <hr/>
+            <select value ={value.select} name="select" onChange={handleChange}>
+               <option value='Seleccione'>--Seleccione</option>
+                <option value='Chanchazofelix'>Chanchazofelix</option>
+                <option value='Chanchofelix'>Chanchofelix</option>
+                <option value='Chanchitofelix'>Chanchitofelix</option>
+                <option value='Chanchitotriste'>Chanchitotriste</option>
+            </select>
+            <hr/>
+            <input type ="checkbox"
+            name="check"
+            onChange={handleChange}
+            checked={value.check}
+            />
+            <hr/>
+            <div> 
+
+                <label> Chancho</label>
+                <input   onChange={handleChange} type="radio" value="feliz" name="estado" checked={value.estado ==='feliz' }/>Feliz
+                <input   onChange={handleChange}  type="radio" value="triste" name="estado" checked={value.estado ==='triste'}  />Triste
+                <input    onChange={handleChange} type="radio" value="asustado"  name="estado" checked={value.estado ==='asustado'} />Asustado
+
+            </div>
+        </div>
+
+    )
 }
 
-export default App;
+export default App
---
+++ src/uncontrolled-component.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+//formulario Uncontrolled
+  const submit = (e)=>{
+
+   const data=Array.from(new FormData(e.target))
+   console.log(Object.fromEntries(data))
+  }
+  return (
+    <form onSubmit={submit}>
+      <div>
+      <span>
+        <p>Prueba formulario </p>
+      </span>
+      <input name='campo' />
+      </div>
+    <input name='campo-2' />
+    <input type='submit' value='Enviar' />
+    </form>
+    
+    );
+}
+
+export default App;
